f82b5042365a9d80bc4a134dc784eb5198d14690
daniellumertz <daniellumertz2@gmail.com>
2022-06-08 01:16:17 -0300

    add goto to iterate function

--- Testing Functions.lua
+++ Testing Functions.lua
@@ -42,42 +42,45 @@ function IterateMIDI(MIDIstring,miditype,ch,selected,muted,filter_midiend,start,
         while iteration_stringPos < MIDIlen do 
             local offset, flags, ms, stringPos = string.unpack("i4Bs4", MIDIstring, iteration_stringPos)
             iteration_stringPos = stringPos
-
-            local cond = true -- if cond true then return iterator
             
-            if miditype and cond then -- check midi type . 
+            -- check midi type .
+            if miditype then 
                 local msg_type = ms:byte(1)>>4 -- moves last 4 bit into void. Rest only the first 4 bits that define midi type
                 if type(miditype) == "table" then -- if miditype is a table with all types to get
-                    cond = TableHaveValue(miditype, msg_type) 
+                    if not TableHaveValue(miditype, msg_type) then goto continue end 
                 else -- if is just a value
-                    cond = msg_type == miditype 
+                    if not (msg_type == miditype) then goto continue end 
                 end
             end
-
-            if ch and cond then -- check channel. 
+            
+            -- check channel.
+            if ch then  
                 local msg_ch = ms:byte(1)&0x0F -- 0x0F = 0000 1111 in binary . ms is decimal. & is an and bitwise operation "have to have 1 in both to be 1". Will return channel as a decimal number. 0 based
                 msg_ch = msg_ch + 1 -- makes it 1 based
                 if type(ch) == "table" then -- if ch is a table with all ch to get
-                    cond = TableHaveValue(ch, msg_ch) 
+                    if not TableHaveValue(ch, msg_ch) then goto continue end 
                 else -- if is just a value
-                    cond = msg_ch == ch
+                    if not (msg_ch == ch) then goto continue end 
                 end
             end
 
-            if selected ~= nil and cond then
+            -- check selected
+            if selected ~= nil then
                 local msg_sel = tonumber(ToBits(flags):sub(1,1)) == 1 -- ToBits(flags) return flags as a binary number. Each option is a number from left to right: selected, mute. EX: selected not muted = 10 selected and muted = 11. :sub(1,1) get only one of those values.tonumber() == 1 transforms in boolean.  
-                cond = msg_sel == selected
+                if not (msg_sel == selected) then goto continue end
             end
 
-            if muted ~= nil and cond then
-                print('here')
+            -- check muted
+            if muted ~= nil then
                 local msg_mute = tonumber(ToBits(flags):sub(2,2)) == 1 -- ToBits(flags) return flags as a binary number. Each option is a number from left to right: selected, mute. EX: selected not muted = 10 selected and muted = 11. :sub(1,1) get only one of those values.tonumber() == 1 transforms in boolean.  
-                cond = msg_mute == muted
+                if not (msg_mute == muted) then goto continue end
             end
 
-            if cond then -- if cond didn't change to false return 
+            if true then -- hm I cant just put return in the middle of a function. But I decided to use goto as lua dont have continue. and if it is here it is allright. so if true then return end  
                 return  offset, flags, ms, stringPos
             end
+
+            ::continue::
         end 
         return nil -- Ends the iteration
     end    
@@ -86,7 +89,7 @@ end
 
 for take in enumMIDITakes(midi_editor, true) do
     local retval, MIDIstring = reaper.MIDI_GetAllEvts(take, "")
-    for offset, flags, ms, stringPos in IterateMIDI(MIDIstring,{8,11},{1,3},nil,nil,true,start,backwards,step) do
+    for offset, flags, ms, stringPos in IterateMIDI(MIDIstring,nil,{4,2,1},nil,nil,true) do
         print('offset    :   '..offset)
         print('flags    :   '..flags)
         print('    ')
