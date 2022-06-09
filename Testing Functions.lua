0b0d117379797c9358a1bb66cf7439a687b26b72
daniellumertz <daniellumertz2@gmail.com>
2022-06-09 20:35:27 -0300

    1x proxess

--- Testing Functions.lua
+++ Testing Functions.lua
@@ -14,17 +14,15 @@ local midi_editor  = reaper.MIDIEditor_GetActive()
 for take in enumMIDITakes(midi_editor, true) do
     local retval, MIDIstr = reaper.MIDI_GetAllEvts(take)
     local midi_table = CreateMIDITable(MIDIstr)
-    for i2 = 1, 10 do
-        for i = 1, #midi_table do
-            local type,ch,val1,val2,all = UnpackMIDIMessage(midi_table[i].msg)
+    for i = 1, #midi_table do
+        local type,ch,val1,val2,all = UnpackMIDIMessage(midi_table[i].msg)
 
-            if type == 9 or type == 8 then
-                val1 = val1 +1
-            end    
+        if type == 9 or type == 8 then
+            val1 = val1 +1
+        end    
 
-            local new = PackMIDIMessage(type,ch,val1,val2)
-            midi_table[i].msg = new
-        end
+        local new = PackMIDIMessage(type,ch,val1,val2)
+        midi_table[i].msg = new
     end
     local midi_packed = PackMIDITable(midi_table)
     reaper.MIDI_SetAllEvts(take, midi_packed)
