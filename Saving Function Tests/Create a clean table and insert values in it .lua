7ab35efedc4c5509a50bafb590dfbe7db5654068
daniellumertz <daniellumertz2@gmail.com>
2022-06-10 04:44:02 -0300

    exemples for insert

--- Saving Function Tests/Create a clean table and insert values in it .lua
+++ Saving Function Tests/Create a clean table and insert values in it .lua
@@ -16,19 +16,22 @@ for take in enumMIDITakes(midi_editor, true) do
     local midi_table = CreateMIDITable(MIDIstr)
     local cnt = 0
     local new_midi_table = {}
-    for i = 1, #midi_table  do
-        if midi_table[i].flags.selected and midi_table[i].msg.val1 ~= 123 then
-            InsertMIDI(new_midi_table,960*cnt,midi_table[i].msg,midi_table[i].flags)
-            cnt = cnt + 1 
-        else 
+    for i = 1, #midi_table  do 
+        if midi_table[i].flags.selected and midi_table[i].msg.val1 ~= 123 then -- If is selected insert it and a copy of it 1qt note latter
             InsertMIDI(new_midi_table,midi_table[i].offset_count,midi_table[i].msg,midi_table[i].flags)
+            InsertMIDI(new_midi_table,midi_table[i].offset_count+960,midi_table[i].msg,midi_table[i].flags)
+        else 
+            InsertMIDI(new_midi_table,midi_table[i].offset_count,midi_table[i].msg,midi_table[i].flags) -- Other value
         end
-
     end
     local midi_packed = PackMIDITable(new_midi_table)
     reaper.MIDI_SetAllEvts(take, midi_packed)
 end
+--print(cnt)
 
+--[[             print(960*cnt)
+SetMIDI(midi_table,i,960*cnt,nil,nil)
+cnt = cnt + 1 ]]
 
 print(reaper.time_precise() - start)
 print('----------------')
